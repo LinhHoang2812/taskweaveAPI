f57b8d692fbefec1a8d564aa4a74f4104f44fc1d
LinhHoang2812 <zoeylinh2812@gmail.com>
2024-04-15 18:04:18 +0200

    create only server

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,6 @@
 # exit on error
 set -o errexit
 
+bundle lock --add-platform x86_64-linux
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
