1b983951a53d5f55be760b5ec85e4a6545929cc6
LinhHoang2812 <zoeylinh2812@gmail.com>
2024-04-16 15:11:46 +0200

    fix project serializer

--- app/serializers/api/v1/projects_serializer.rb
+++ app/serializers/api/v1/projects_serializer.rb
@@ -1,7 +1,9 @@
 class Api::V1::ProjectsSerializer < ActiveModel::Serializer
   attributes :id,:title,:user_id
-  has_many :sections
-  has_many :tasks
+  has_many :sections, serializer: Api::V1::SectionsSerializer
+  
+
+  
   
    
 end
