bd88f319cf68b9d2c1b5094b9d4b534a9bad8910
LinhHoang2812 <zoeylinh2812@gmail.com>
2024-04-16 15:38:01 +0200

    fix project serializer

--- app/serializers/api/v1/projects_serializer.rb
+++ app/serializers/api/v1/projects_serializer.rb
@@ -1,8 +1,17 @@
 class Api::V1::ProjectsSerializer < ActiveModel::Serializer
-  attributes :id,:title,:user_id
-  has_many :sections, serializer: Api::V1::SectionsSerializer
+  attributes :id,:title,:user_id,:sections
+  #has_many :sections, serializer: Api::V1::SectionsSerializer
   
-
+  def sections 
+    sections=[]
+    object.sections.each do |section|
+      section_obj = section.as_json
+      section_obj[:tasks]= section.tasks.sort_by { |task| task[:position]}
+      sections.append(section_obj)
+    end
+    sections = sections.sort_by {|section| section["position"]}
+    sections
+  end
   
   
    
